50f1fd6548680390cd13ba84ca77720301ffc01f
Leo_Upsy <leopold.andre@edu.ece.fr>
2026-01-16 14:21:58 +0100

    exos entrainement

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,27 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+
+  // Algo tri décroissant
+  List<int> numbers = [25, 42, 79, 12];
+  for(int i=0; i<numbers.length; i++){
+    for(int j=i+1; j<numbers.length-i; j++){
+      if(numbers[i]<numbers[j]){
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
+  }
+
+  //mutl par 2 avec map
+  Iterable<int> l2 = numbers.map((int n) {return n*2;});
+
+  //retour nombres pairs
+  List<int> l3 = numbers.where((n)=> n%2==0).toList();
+  
+
+  print(numbers);
+  print(l2);
+  print(l3);
 }
