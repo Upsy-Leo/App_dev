54a41de44a31156f9690c76033a7d39b64eb1d04
Leo_Upsy <leopold.andre@edu.ece.fr>
2026-01-21 22:46:42 +0100

    ajout widget email et body

--- Cours_1_TP_1/lib/login/login_page.dart
+++ Cours_1_TP_1/lib/login/login_page.dart
@@ -5,9 +5,52 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mailController = TextEditingController();
+
     return Scaffold(appBar: AppBar.new(
       title: const Text("Log in or Sign up", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w700)),
-      )
-      );
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Container(
+                child: Form(child: Column(
+                  children: [
+                    Email(mailController)
+                  ],
+                )),
+              )
+            ],
+        ),
+      ),
+    );
   }
+  
+}
+
+class Email extends StatelessWidget{
+
+  final TextEditingController mail;
+
+  const Email(this.mail);
+
+  @override
+  Widget build(BuildContext context){
+    return TextFormField(
+      controller: mail,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        prefixIcon: const Icon(Icons.email_outlined),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        filled: true,
+        fillColor: Colors.grey,
+      ),
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+    );
+  }
+
 }
